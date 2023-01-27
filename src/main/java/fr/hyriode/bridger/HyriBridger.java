6851161ac49f794539210373006367b94fd082d0
Caliix <calyxadresse@gmail.com>
2023-01-27 23:24:20 +0100

    ♻️Refactor code

--- src/main/java/fr/hyriode/bridger/HyriBridger.java
+++ src/main/java/fr/hyriode/bridger/HyriBridger.java
@@ -15,6 +15,7 @@ import org.bukkit.Location;
 import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import javax.xml.ws.Provider;
 import java.util.logging.Level;
 
 public class HyriBridger extends JavaPlugin {
@@ -45,20 +46,20 @@ public class HyriBridger extends JavaPlugin {
 
         if (HyriAPI.get().getConfig().isDevEnvironment()) {
             this.configuration = new BridgerConfig(
-//  Spawn location on the first island
+             //  Spawn location on the first island
             new LocationWrapper(new Location(IHyrame.WORLD.get(), 0.5, 100.0, 0.5, 180.0F, 0.0F)),
-// NPC Location on the first island
-                new LocationWrapper(new Location(IHyrame.WORLD.get(), -3.5, 100.0, 1.5, -90.0F, 0.0F)),
-// Hologram Location on the first island
-                new LocationWrapper(new Location(IHyrame.WORLD.get(), 2.0, 102.0, 5.0, 0.0F, 0.0F)),
-// Pos1 of island area
-                new LocationWrapper(new Location(IHyrame.WORLD.get(), -6, 107.0, -1.0)),
-// Pos2 of island area
-                new LocationWrapper(new Location(IHyrame.WORLD.get(), 6.0, 96, -52.0)),
-// Y coordinates where the player respawn
-                97.0,
-// Distance between 2 islands
-                new LocationWrapper(new Location(IHyrame.WORLD.get(), 25, 0.0,0)));
+            // NPC Location on the first island
+            new LocationWrapper(new Location(IHyrame.WORLD.get(), -3.5, 100.0, 1.5, -90.0F, 0.0F)),
+            // Hologram Location on the first island
+            new LocationWrapper(new Location(IHyrame.WORLD.get(), 2.0, 102.0, 5.0, 0.0F, 0.0F)),
+            // Pos1 of island area
+            new LocationWrapper(new Location(IHyrame.WORLD.get(), -6, 107.0, -1.0)),
+            // Pos2 of island area
+            new LocationWrapper(new Location(IHyrame.WORLD.get(), 6.0, 96, -52.0)),
+            // Y coordinates where the player respawn
+            97.0,
+            // Distance between 2 islands
+            new LocationWrapper(new Location(IHyrame.WORLD.get(), 25, 0.0,0)));
         } else this.configuration = HyriAPI.get().getServer().getConfig(BridgerConfig.class);
 
         this.hyrame = HyrameLoader.load(new BridgerProvider(this));
