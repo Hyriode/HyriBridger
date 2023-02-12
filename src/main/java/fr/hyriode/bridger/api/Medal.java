186ae57f7c7544337a8ed6172ed91d68f7cea910
Pas2Pseudo <williamjolivet74@gmail.com>
2023-02-12 23:15:28 +0100

    Update Medal.java

--- src/main/java/fr/hyriode/bridger/api/Medal.java
+++ src/main/java/fr/hyriode/bridger/api/Medal.java
@@ -1,6 +1,7 @@
 package fr.hyriode.bridger.api;
 
-import com.avaje.ebeaninternal.server.lib.util.NotFoundException;
+import java.util.HashMap;
+import java.util.Map;
 
 public enum Medal {
 
@@ -9,6 +10,13 @@ public enum Medal {
     GOLD(3, 6500, 12000, 5300, "medal.gold"),
     ULTIMATE(4, 4300, 8500, 4300, "medal.ultimate")
     ;
+
+    public static final Map<Integer, Medal> BY_ID = new HashMap<>();
+    static {
+        for (Medal medal : values()) {
+            BY_ID.put(medal.id, medal);
+        }
+    }
 
     private final int id;
     private final long timeToReachShort;
@@ -25,12 +33,7 @@ public enum Medal {
     }
 
     public static Medal getById(int id) {
-        for (Medal medal : Medal.values()) {
-            if (medal.getId() == id) {
-                return medal;
-            }
-        }
-        throw new NotFoundException("not found");
+        return BY_ID.get(id);
     }
 
     public long getTimeToReach(String gameType) {
