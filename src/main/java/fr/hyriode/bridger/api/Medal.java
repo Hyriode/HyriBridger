d36477b7c60e4bd42f2c47d7f371e4e342db4c84
Pas2Pseudo <williamjolivet74@gmail.com>
2023-02-12 23:13:33 +0100

    Update Medal.java

--- src/main/java/fr/hyriode/bridger/api/Medal.java
+++ src/main/java/fr/hyriode/bridger/api/Medal.java
@@ -4,23 +4,23 @@ import com.avaje.ebeaninternal.server.lib.util.NotFoundException;
 
 public enum Medal {
 
-    BRONZE(12000, 44000, 8500, 1, "medal.bronze"),
-    IRON(9000, 24000, 6700, 2, "medal.iron"),
-    GOLD(6500, 12000, 5300, 3, "medal.gold"),
-    ULTIMATE(4300, 8500, 4300, 4, "medal.ultimate")
+    BRONZE(1, 12000, 44000, 8500, "medal.bronze"),
+    IRON(2, 9000, 24000, 6700, "medal.iron"),
+    GOLD(3, 6500, 12000, 5300, "medal.gold"),
+    ULTIMATE(4, 4300, 8500, 4300, "medal.ultimate")
     ;
 
+    private final int id;
     private final long timeToReachShort;
     private final long timeToReachNormal;
     private final long timeToReachDiagonal;
-    private final int id;
     private final String languageValue;
 
-    Medal(long timeToReachShort, long timeToReachLong, long timeToReachDiagonal, int id, String languageValue) {
+    Medal(int id, long timeToReachShort, long timeToReachLong, long timeToReachDiagonal, String languageValue) {
+        this.id = id;
         this.timeToReachShort = timeToReachShort;
         this.timeToReachNormal = timeToReachLong;
         this.timeToReachDiagonal = timeToReachDiagonal;
-        this.id = id;
         this.languageValue = languageValue;
     }
 
