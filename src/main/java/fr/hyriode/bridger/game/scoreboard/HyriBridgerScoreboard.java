44859e8f9a1bef520b6ec03719aba6ad0e31b878
WilliamJolivet <80533655+Pas2Pseudo@users.noreply.github.com>
2023-02-20 16:45:47 +0100

    Update HyriBridgerScoreboard.java

--- src/main/java/fr/hyriode/bridger/game/scoreboard/HyriBridgerScoreboard.java
+++ src/main/java/fr/hyriode/bridger/game/scoreboard/HyriBridgerScoreboard.java
@@ -23,25 +23,24 @@ public class HyriBridgerScoreboard extends HyriGameScoreboard<BridgerGame> {
         this.plugin = plugin;
         this.player = player;
         this.gamePlayer = this.plugin.getGame().getPlayer(player);
-
-        this.addLines();
+        addLines();
     }
 
     public void addLines() {
-        this.setLine(0, this.getDateLine(), line -> line.setValue(this.getDateLine()), 20*60);
-        this.addBlankLine(1);
-        this.setLine(2, ChatColor.AQUA + "" + ChatColor.BOLD + this.getValue("scoreboard.best-time"));
-        this.setLine(3, this.getBestTime(), line -> line.setValue(this.getBestTime()), 20);
-        this.setLine(4, this.getActualTime(), line -> line.setValue(this.getActualTime()), 1);
-        this.addBlankLine(5);
-        this.setLine(6, ChatColor.DARK_AQUA + this.getValue("scoreboard.top-3"));
-        this.setLine(7, this.getBestTimes(1), line -> line.setValue(this.getBestTimes(1)), 20);
-        this.setLine(8, this.getBestTimes(2), line -> line.setValue(this.getBestTimes(2)), 20);
-        this.setLine(9, this.getBestTimes(3), line -> line.setValue(this.getBestTimes(3)), 20);
-        this.addBlankLine(10);
-        this.setLine(11, this.getMedalLine(), line -> line.setValue(this.getMedalLine()), 20);
-        this.addBlankLine(12);
-        this.addHostnameLine();
+        setLine(0, getDateLine(), line -> line.setValue(getDateLine()), 20 * 60);
+        addBlankLine(1);
+        setLine(2, ChatColor.AQUA + "" + ChatColor.BOLD + getValue("scoreboard.best-time"));
+        setLine(3, getBestTime(), line -> line.setValue(getBestTime()), 20);
+        setLine(4, getActualTime(), line -> line.setValue(getActualTime()), 1);
+        addBlankLine(5);
+        setLine(6, ChatColor.DARK_AQUA + getValue("scoreboard.top-3"));
+        setLine(7, getBestTimes(1), line -> line.setValue(getBestTimes(1)), 20);
+        setLine(8, getBestTimes(2), line -> line.setValue(getBestTimes(2)), 20);
+        setLine(9, getBestTimes(3), line -> line.setValue(getBestTimes(3)), 20);
+        addBlankLine(10);
+        setLine(11, getMedalLine(), line -> line.setValue(getMedalLine()), 20);
+        addBlankLine(12);
+        addHostnameLine();
     }
 
     private String getDateLine() {
@@ -49,39 +48,24 @@ public class HyriBridgerScoreboard extends HyriGameScoreboard<BridgerGame> {
     }
 
     private String getBestTime() {
-        if (this.gamePlayer.getPB() != null) {
-            return ChatColor.YELLOW + this.gamePlayer.getPB().toFormattedTime();
-        }
-        return ChatColor.GRAY + "-.---";
+        return (this.gamePlayer.getPB() != null) ? ChatColor.YELLOW + this.gamePlayer.getPB().toFormattedTime() : ChatColor.GRAY + "-.---";
     }
 
     private String getActualTime() {
-        String startString = ChatColor.WHITE + this.getValue("scoreboard.actual-time") + " " + ChatColor.YELLOW;
-        if (this.gamePlayer.isBridging()) {
-            if (this.gamePlayer.getActualTimer() != null) {
-                if (this.gamePlayer.getActualTimer().getFormattedActualTime() != null) {
-                    return startString + this.gamePlayer.getActualTimer().getFormattedActualTime();
-                }
-            }
-        }
-        return startString + "0.000";
+        String startString = ChatColor.WHITE + getValue("scoreboard.actual-time") + " " + ChatColor.YELLOW;
+        return (this.gamePlayer.isBridging() && this.gamePlayer.getActualTimer() != null && this.gamePlayer.getActualTimer().getFormattedActualTime() != null) ? startString + this.gamePlayer.getActualTimer().getFormattedActualTime() : startString + "0.000";
     }
 
     private String getBestTimes(int i) {
-        return this.plugin.getGame().getSession().getFormattedTop(i);
+        return plugin.getGame().getSession().getFormattedTop(i);
     }
 
     private String getMedalLine() {
         final Medal medal = this.gamePlayer.getMedal();
-        if (medal != null) {
-            return ChatColor.GOLD + this.getValue("scoreboard.medal.actual") + this.getValue(medal.getLanguageValue());
-        } else {
-            return ChatColor.GOLD + this.getValue("scoreboard.medal.actual") + ChatColor.RED + "✘";
-        }
+        return ChatColor.GOLD + getValue("scoreboard.medal.actual") + ((medal != null) ? getValue(medal.getLanguageValue()) : ChatColor.RED + "✘");
     }
 
     private String getValue(String key) {
-        return HyriLanguageMessage.get(key).getValue(this.player);
+        return HyriLanguageMessage.get(key).getValue(player);
     }
-
 }
