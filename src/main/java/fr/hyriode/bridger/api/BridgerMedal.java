34f8f6db2dde8920529fc2a8518757d2c185dae2
WilliamJolivet <80533655+Pas2Pseudo@users.noreply.github.com>
2023-02-27 16:21:56 +0100

    add rank reward block

--- src/main/java/fr/hyriode/bridger/api/BridgerMedal.java
+++ src/main/java/fr/hyriode/bridger/api/BridgerMedal.java
@@ -1,39 +1,48 @@
 package fr.hyriode.bridger.api;
 
 import fr.hyriode.bridger.game.BridgerGameType;
+import fr.hyriode.bridger.game.blocks.BridgerBlock;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
+
+import static fr.hyriode.bridger.game.blocks.BridgerBlock.*;
 
 public enum BridgerMedal {
 
-    BRONZE("medal.bronze", new HashMap<BridgerGameType, Long>(){{
+    BRONZE("medal.bronze", COAL_BLOCK, new HashMap<BridgerGameType, Long>(){{
         put(BridgerGameType.SHORT, 12000L);
         put(BridgerGameType.NORMAL, 44000L);
         put(BridgerGameType.DIAGONAL, 8500L);
     }}),
-    IRON("medal.iron", new HashMap<BridgerGameType, Long>(){{
+    IRON("medal.iron", IRON_BLOCK, new HashMap<BridgerGameType, Long>(){{
         put(BridgerGameType.SHORT, 9000L);
         put(BridgerGameType.NORMAL, 24000L);
         put(BridgerGameType.DIAGONAL, 7000L);
     }}),
-    GOLD("medal.gold", new HashMap<BridgerGameType, Long>(){{
+    GOLD("medal.gold", GOLD_BLOCK, new HashMap<BridgerGameType, Long>(){{
         put(BridgerGameType.SHORT, 6000L);
         put(BridgerGameType.NORMAL, 12000L);
         put(BridgerGameType.DIAGONAL, 5500L);
     }}),
-    ULTIMATE("medal.ultimate", new HashMap<BridgerGameType, Long>(){{
+    ULTIMATE("medal.ultimate", DIAMOND_BLOCK, new HashMap<BridgerGameType, Long>(){{
         put(BridgerGameType.SHORT, 4500L);
         put(BridgerGameType.NORMAL, 9000L);
         put(BridgerGameType.DIAGONAL, 7000L);
     }});
 
-    private final Map<BridgerGameType, Long> timeToReach;
     private final String languageValue;
+    private BridgerBlock rewardBlock;
+    private final Map<BridgerGameType, Long> timeToReach;
 
     BridgerMedal(String languageValue, Map<BridgerGameType, Long> timeToReach) {
-        this.timeToReach = timeToReach;
         this.languageValue = languageValue;
+        this.timeToReach = timeToReach;
+    }
+
+    BridgerMedal(String languageValue, BridgerBlock rewardBlock, Map<BridgerGameType, Long> timeToReach) {
+        this.languageValue = languageValue;
+        this.timeToReach = timeToReach;
+        this.rewardBlock = rewardBlock;
     }
 
     public long getTimeToReach(BridgerGameType gameType) {
@@ -42,5 +51,9 @@ public enum BridgerMedal {
 
     public String getLanguageValue() {
         return languageValue;
+    }
+
+    public BridgerBlock getRewardBlock() {
+        return rewardBlock;
     }
 }
