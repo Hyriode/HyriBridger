b862aaeb52e94cc6d617227f953701018b548d4a
Pas2Pseudo <williamjolivet74@gmail.com>
2023-04-20 17:31:18 +0200

    back to the past

--- src/main/java/fr/hyriode/bridger/api/BridgerDuration.java
+++ src/main/java/fr/hyriode/bridger/api/BridgerDuration.java
@@ -1,5 +1,7 @@
 package fr.hyriode.bridger.api;
 
+import java.text.DecimalFormat;
+
 public class BridgerDuration implements Comparable<BridgerDuration> {
 
     private final long ms;
@@ -8,14 +10,23 @@ public class BridgerDuration implements Comparable<BridgerDuration> {
         this.ms = timeInMs;
     }
 
+    public long toSecondsPart() {
+        return (this.ms / 1000) % 1000;
+    }
+
+    public long toMillisPart() {
+        return this.ms  % 1000;
+    }
+
     public String toFormattedTime() {
-        double durationInSeconds = ms / 1000.0;
+        return this.toSecondsPart() + "." + new DecimalFormat("000").format(this.toMillisPart());
+        /*float durationInSeconds = (float) (ms / 1000.0);
         double roundedNumber = Math.round(durationInSeconds * 20) / 20.0;
         String result = String.valueOf(roundedNumber);
         if (result.split("\\.")[1].length() == 1) {
             result += "0";
         }
-        return result + "0";
+        return result + "0";*/
     }
 
     public long getExactTime() {
