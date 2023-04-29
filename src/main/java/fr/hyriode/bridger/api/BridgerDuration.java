b10c2921a755d1ccc8b33cb3a440c773984120a8
Pas2Pseudo <williamjolivet74@gmail.com>
2023-04-29 15:23:39 +0200

    Update BridgerDuration.java

--- src/main/java/fr/hyriode/bridger/api/BridgerDuration.java
+++ src/main/java/fr/hyriode/bridger/api/BridgerDuration.java
@@ -1,38 +1,5 @@
 package fr.hyriode.bridger.api;
 
-//import java.text.DecimalFormat;
-//
-//public class BridgerDuration implements Comparable<BridgerDuration> {
-//
-//    private final long ms;
-//
-//    public BridgerDuration(long timeInMs) {
-//        this.ms = timeInMs;
-//    }
-//
-//    public long toSecondsPart() {
-//        return (this.ms / 1000) % 1000;
-//    }
-//
-//    public long toMillisPart() {
-//        return this.ms  % 1000;
-//    }
-//
-//    public String toFormattedTime() {
-//        return this.toSecondsPart() + "." + new DecimalFormat("000").format(this.toMillisPart());
-//    }
-//
-//    public long getExactTime() {
-//        return ms;
-//    }
-//
-//    @Override
-//    public int compareTo(BridgerDuration other) {
-//        return Long.compare(this.getExactTime(), other.getExactTime());
-//    }
-//}
-
-
 import java.text.DecimalFormat;
 
 public class BridgerDuration implements Comparable<BridgerDuration> {
@@ -48,19 +15,11 @@ public class BridgerDuration implements Comparable<BridgerDuration> {
     }
 
     public long toMillisPart() {
-        return this.ms % 1000;
+        return this.ms  % 1000;
     }
 
     public String toFormattedTime() {
-        return toFormattedTimeWithRounding();
-//        return this.toSecondsPart() + "." + new DecimalFormat("000").format(this.toMillisPart());
-    }
-
-    public String toFormattedTimeWithRounding() {
-        long roundedDurationInMillis = roundToNearestFiveCents(this.ms);
-        long roundedSecondsPart = (roundedDurationInMillis / 1000) % 1000;
-        long roundedMillisPart = roundedDurationInMillis % 1000;
-        return roundedSecondsPart + "." + new DecimalFormat("000").format(roundedMillisPart);
+        return this.toSecondsPart() + "." + new DecimalFormat("000").format(this.toMillisPart());
     }
 
     public long getExactTime() {
@@ -70,11 +29,5 @@ public class BridgerDuration implements Comparable<BridgerDuration> {
     @Override
     public int compareTo(BridgerDuration other) {
         return Long.compare(this.getExactTime(), other.getExactTime());
-    }
-
-    public static long roundToNearestFiveCents(long durationInMillis) {
-        long durationInSeconds = durationInMillis / (long) 1000.0;
-        long roundedDurationInSeconds = (long) (Math.round(durationInSeconds * 20.0) / 20.0);
-        return roundedDurationInSeconds * 1000;
     }
 }
